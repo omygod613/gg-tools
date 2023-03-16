4184d37d257bd321fa1a355da27490df959a7512
omygod613 <omygod613@gmail.com>
2023-03-16 11:37:49 +0800

    fix: some script

--- script/install_airbyte.sh
+++ script/install_airbyte.sh
@@ -2,6 +2,11 @@
 
 BASE_DIR=$(dirname "$0")
 
-bash deployment/mysql/install.sh
+kubectl create ns devns3
+kubectl config set-context --current --namespace devns3
+
 bash deployment/airbyte/install.sh
+bash deployment/mysql/install.sh
 bash deployment/mariadb/install.sh
+bash deployment/mssql/install.sh
+bash deployment/oracle/install.sh
--- tools/autocompletion/install.sh
+++ tools/autocompletion/install.sh
@@ -6,3 +6,4 @@ sudo apt-get install bash-completion
 source /usr/share/bash-completion/bash_completion
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 source ~/.bashrc
+source /usr/share/bash-completion/bash_completion
