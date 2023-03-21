191f00f0f71887c7981f206a30c4cdbc58f8d928
omygod613 <omygod613@gmail.com>
2023-03-21 16:05:26 +0800

    update some script for mssql

--- script/install_airbyte.sh
+++ script/install_airbyte.sh
@@ -9,5 +9,5 @@ bash deployment/airbyte/install.sh
 bash deployment/mysql/install.sh
 bash deployment/mariadb/install.sh
 bash deployment/mssql/install.sh
-bash deployment/oracle/install.sh
+# bash deployment/oracle/install.sh
 bash deployment/minio/install.sh
