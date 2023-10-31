840422df503c92ec4e770c732941dee9c5d8d362
omygod613 <omygod613@gmail.com>
2023-10-31 09:13:38 +0800

    fix: docker image cve

--- deployment/dolphinscheduler/docker/build.sh
+++ deployment/dolphinscheduler/docker/build.sh
@@ -2,13 +2,19 @@
 
 BASE_DIR=$(dirname "$0")
 
-VERSION="3.2.0-1"
+VERSION="3.2.0-2"
 
-sh dolphinscheduler-alert-server/build.sh
-sh dolphinscheduler-api/build.sh
-sh dolphinscheduler-master/build.sh
-sh dolphinscheduler-tools/build.sh
-sh dolphinscheduler-worker/build.sh
+docker build -t omygod613/dolphinscheduler-alert-server:${VERSION} $BASE_DIR/dolphinscheduler-alert-server/.
+docker build -t omygod613/dolphinscheduler-api:${VERSION} $BASE_DIR/dolphinscheduler-api/.
+docker build -t omygod613/dolphinscheduler-master:${VERSION} $BASE_DIR/dolphinscheduler-master/.
+docker build -t omygod613/dolphinscheduler-tools:${VERSION} $BASE_DIR/dolphinscheduler-tools/.
+docker build -t omygod613/dolphinscheduler-worker:${VERSION} $BASE_DIR/dolphinscheduler-worker/.
+
+# kind load docker-image omygod613/dolphinscheduler-alert-server:${VERSION}
+# kind load docker-image omygod613/dolphinscheduler-api:${VERSION}
+# kind load docker-image omygod613/dolphinscheduler-master:${VERSION}
+# kind load docker-image omygod613/dolphinscheduler-tools:${VERSION}
+# kind load docker-image omygod613/dolphinscheduler-worker:${VERSION}
 
 docker push omygod613/dolphinscheduler-alert-server:${VERSION}
 docker push omygod613/dolphinscheduler-api:${VERSION}
