9b72ce879def3114d977c9c373024b9ef7d8af92
I-Shen <omygod613@gmail.com>
2024-07-02 15:54:38 +0800

    chore: add mongodb dbz and connector to kafka connect (#18)

--- deployment/kafka-connect/docker/build.sh
+++ deployment/kafka-connect/docker/build.sh
@@ -2,10 +2,10 @@
 
 BASE_DIR=$(dirname "$0")
 
-VERSION="7.6.1-20240605-2"
+VERSION="7.6.1-20240702"
 
 docker build -t omygod613/cp-kafka-connect:${VERSION} $BASE_DIR/.
 
 # kind load docker-image omygod613/cp-kafka-connect:${VERSION}
 
-docker push omygod613/cp-kafka-connect:${VERSION}
+# docker push omygod613/cp-kafka-connect:${VERSION}
