f0c5cc37069f96fdcc0dca2d5f86ade1c57d6d15
isliao613 <isliao613@gmail.com>
2025-12-24 16:58:50 +0800

    tmp

--- script/port-forward.sh
+++ script/port-forward.sh
@@ -2,6 +2,4 @@
 
 BASE_DIR=$(dirname "$0")
 
-kubectl port-forward svc/isliao-kafka-connect-cp-kafka-connect 8083:8083
-kubectl port-forward svc/isliao-kafka-connect-ui 80:80
-kubectl port-forward svc/isliao-airbyte-airbyte-webapp-svc 80:80
+kubectl port-forward svc/isliao-kafka-connect-ui 80:8000
