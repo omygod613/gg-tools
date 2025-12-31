c2210bf50c8400222a37fde3e874934b53d1dde9
isliao613 <isliao613@gmail.com>
2025-12-31 14:40:24 +0800

    feat: add Oracle CDC to MariaDB E2E test infrastructure
    - Add Makefile for automated E2E testing with Oracle XE 21c
    - Add Oracle XE deployment (Debezium 2.1.x compatible)
    - Add Debezium Oracle source and JDBC sink connector configs
    - Add SQL setup scripts for Oracle CDB/PDB and MariaDB
    - Fix Kafka single-broker replication factor configuration
    - Update CDC user to c##dbzcdc with CONTAINER=ALL
    - Remove deprecated log.mining.continuous.mine (Oracle 21c)
    - Update Kafka Connect image to 7.6.1

--- script/port-forward.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASE_DIR=$(dirname "$0")
-
-kubectl port-forward svc/isliao-kafka-connect-cp-kafka-connect 8083:8083
-kubectl port-forward svc/isliao-kafka-connect-ui 80:80
-kubectl port-forward svc/isliao-airbyte-airbyte-webapp-svc 80:80
