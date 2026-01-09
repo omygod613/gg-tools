701fffbe5e84ca8c697a82ac3275c9dddb5a3f13
I-Shen <isliao613@gmail.com>
2026-01-09 13:49:44 +0800

    chore: remove unused SQL and connector config files (#34)
    Remove files not referenced in the codebase:
    - hack/sql/mariadb-setup.sql
    - hack/sql/oracle-free-setup.sql
    - hack/source-debezium/oracle-xe-demo.json

--- hack/sql/mariadb-setup.sql
+++
@@ -1,3 +0,0 @@
--- MariaDB setup for CDC sink
-CREATE DATABASE IF NOT EXISTS target_database DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-DROP TABLE IF EXISTS target_database.target_orders;
